0e0e0fc6fd949636af0c10c56237c39be37774c1
Alexander Pacha <alexander.pacha@gmail.com>
2017-05-12 15:08:41 +0200

    Create export code that generates an image from the data

--- HomusImageGenerator/Program.cs
+++ HomusImageGenerator/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
 
 namespace HomusImageGenerator
 {
-    internal class Program
+    internal partial class Program
     {
         private static void Main(string[] args)
         {
@@ -30,84 +28,27 @@ namespace HomusImageGenerator
                 }
 
                 var symbolFiles = GetPathToAllSymbolFiles(databaseDirectory);
+                var baseDirectoryForExport = Path.GetFullPath(Path.Combine("..", "..", "..", "HOMUS", "Export"));
                 Console.WriteLine($"Processing {symbolFiles.Count} symbols...");
-
-                int minX = int.MaxValue;
-                int maxX = 0;
-                int minY = int.MaxValue;
-                int maxY = 0;
 
                 foreach (var symbolFile in symbolFiles)
                 {
                     var content = File.ReadAllText(symbolFile);
                     Symbol symbol = Symbol.InitializeFromString(content);
 
-                    foreach (var stroke in symbol.Strokes)
+                    var targetDirectory = Path.Combine(baseDirectoryForExport, symbol.SymbolName);
+                    if (!Directory.Exists(targetDirectory))
                     {
-                        foreach (var point in stroke)
-                        {
-                            if (point.X < 0)
-                            {
-                                
-                            }
-
-                            maxX = Math.Max(maxX, point.X);
-                            minX = Math.Min(minX, point.X);
-
-                            maxY = Math.Max(maxY, point.Y);
-                            minY = Math.Min(minY, point.Y);
-                        }
-                    }
-                }
-
-                Console.WriteLine($"MinX {minX}, MaxX {maxX}, MinY {minY}, MaxY {maxY}");
-                // Max dimension: 304x448 pixels
-            }
-        }
-
-        private class Symbol
-        {
-            public string FileContent { get; set; }
-            public string SymbolName { get; set; }
-            public List<List<Point>> Strokes { get; set; }
-
-            public Symbol()
-            {
-                Strokes = new List<List<Point>>();
-            }
-
-            public static Symbol InitializeFromString(string content)
-            {
-                var lines = content.Split(new[] { "\n", "\r\n" }, StringSplitOptions.None);
-
-                var newSymbol = new Symbol()
-                {
-                    FileContent = content,
-                    SymbolName = lines[0]
-                };
-                
-                foreach (var strokeString in lines.Skip(1))
-                {
-                    var stroke = new List<Point>();
-                    foreach (var point in strokeString.Split(';').Where(s => s != "")) // Each stroke ends with a ; so the last element after the split should be skipped
-                    {
-                        stroke.Add(new Point(int.Parse(point.Split(',')[0]), int.Parse(point.Split(',')[1])));
+                        Directory.CreateDirectory(targetDirectory);
                     }
 
-                    newSymbol.Strokes.Add(stroke);
-                }
-                
-                return newSymbol;
-            }
+                    var fileName = Path.GetFileNameWithoutExtension(symbolFile);
 
-            public void DrawIntoBitmap()
-            {
-                //Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-                //using (Graphics g = Graphics.FromImage(bmp))
-                //{
-                //    g.DrawLine(new Pen(Color.Red), 0, 0, 10, 10);
-                //}
-                //pictureBox1.Image = bmp;
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        symbol.DrawIntoBitmap(Path.Combine(targetDirectory, $"{fileName}_{i}.png"), i);
+                    }                    
+                }                
             }
         }
 
@@ -115,8 +56,8 @@ namespace HomusImageGenerator
         {
             var symbolFiles = new List<string>();
             foreach (var writerDirectory in Directory.EnumerateDirectories(Path.Combine(databaseDirectory, "HOMUS")))
-            foreach (var symbolFile in Directory.EnumerateFiles(writerDirectory))
-                symbolFiles.Add(symbolFile);
+                foreach (var symbolFile in Directory.EnumerateFiles(writerDirectory))
+                    symbolFiles.Add(symbolFile);
 
             return symbolFiles;
         }
---
+++ HomusImageGenerator/Symbol.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace HomusImageGenerator
+{
+    internal partial class Program
+    {
+        private class Symbol
+        {
+            public string FileContent { get; set; }
+            public string SymbolName { get; set; }
+            public List<List<Point>> Strokes { get; set; }
+            public Rectangle Dimensions { get; set; }
+
+            public Symbol()
+            {
+                Strokes = new List<List<Point>>();
+            }
+
+            public static Symbol InitializeFromString(string content)
+            {
+                var lines = content.Split(new[] { "\n", "\r\n" }, StringSplitOptions.None);
+
+                var newSymbol = new Symbol
+                {
+                    FileContent = content,
+                    SymbolName = lines[0]
+                };
+
+                int minX = int.MaxValue;
+                int maxX = 0;
+                int minY = int.MaxValue;
+                int maxY = 0;
+
+                foreach (var strokeString in lines.Skip(1))
+                {
+                    var stroke = new List<Point>();
+                    foreach (var pointString in strokeString.Split(';').Where(s => s != "")) // Each stroke ends with a ; so the last element after the split should be skipped
+                    {
+                        var point = new Point(int.Parse(pointString.Split(',')[0]), int.Parse(pointString.Split(',')[1]));
+                        stroke.Add(point);
+
+                        maxX = Math.Max(maxX, point.X);
+                        minX = Math.Min(minX, point.X);
+                        maxY = Math.Max(maxY, point.Y);
+                        minY = Math.Min(minY, point.Y);
+                    }
+
+                    newSymbol.Strokes.Add(stroke);
+                    newSymbol.Dimensions = new Rectangle(minX, minY, maxX - minX + 1, maxY - minY + 1);
+                }
+
+                return newSymbol;
+            }
+
+            public void DrawIntoBitmap(string exportFileName, int strokeThickness)
+            {
+                var height = Dimensions.Height;
+                var width = Dimensions.Width;
+                var offset = Dimensions.Location;
+
+                using (var bmp = new Bitmap(width, height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.FillRectangle(new SolidBrush(Color.White), 0, 0, width, height);
+                        foreach (var stroke in Strokes)
+                        {
+                            for (int i = 0; i < stroke.Count - 1; i++)
+                            {
+                                var startPoint = SubtractOffset(stroke[i], offset);
+                                var endPoint = SubtractOffset(stroke[i + 1], offset);
+                                g.DrawLine(new Pen(Color.Black, strokeThickness), startPoint, endPoint);
+                            }
+                        }
+
+                        bmp.Save(exportFileName, ImageFormat.Png);
+                    }
+                }
+            }
+
+            private Point SubtractOffset(Point a, Point b)
+            {
+                return new Point(a.X - b.X, a.Y - b.Y);
+            }
+        }
+    }
+}
