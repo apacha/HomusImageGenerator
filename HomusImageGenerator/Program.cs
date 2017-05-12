f420603e31a7f96f3bc84918c152ec65e73c172d
Alexander Pacha <alexander.pacha@gmail.com>
2017-05-12 11:14:16 +0200

    Initial commit of the console-application

---
+++ HomusImageGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomusImageGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("http://grfia.dlsi.ua.es/homus/HOMUS.zip", Path.Combine("..","..", "..", "HOMUS.zip"));
+            }
+        }
+    }
+}
